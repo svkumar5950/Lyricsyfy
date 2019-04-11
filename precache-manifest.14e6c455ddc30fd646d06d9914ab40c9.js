94565571bdd6977d7d4814528251df1a18b0d009
svkumar5950 <svkumar5950@gmail.com>
2019-04-11 19:56:31 +0200

    Updates

---
+++ precache-manifest.14e6c455ddc30fd646d06d9914ab40c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "040552afc40662a873fe",
+    "url": "/Lyricsyfy/static/js/runtime~main.8baf5ec2.js"
+  },
+  {
+    "revision": "4376f9420bd7472c7169",
+    "url": "/Lyricsyfy/static/js/main.99f828aa.chunk.js"
+  },
+  {
+    "revision": "e6476e9991965bc7b224",
+    "url": "/Lyricsyfy/static/js/2.146256ea.chunk.js"
+  },
+  {
+    "revision": "4376f9420bd7472c7169",
+    "url": "/Lyricsyfy/static/css/main.64e46d9d.chunk.css"
+  },
+  {
+    "revision": "e31c1e690a7985c8da77d7d38596fa07",
+    "url": "/Lyricsyfy/index.html"
+  }
+];
