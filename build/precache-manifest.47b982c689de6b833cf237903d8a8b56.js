c2d42e64df9c48b5494bf93813a97c9114ceb3b6
svkumar5950 <svkumar5950@gmail.com>
2019-04-11 19:31:28 +0200

    Ready for deployment

---
+++ build/precache-manifest.47b982c689de6b833cf237903d8a8b56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "13f4a2c34dbdeb43c0a8",
+    "url": "/static/js/main.9f0e6ba6.chunk.js"
+  },
+  {
+    "revision": "e6476e9991965bc7b224",
+    "url": "/static/js/2.146256ea.chunk.js"
+  },
+  {
+    "revision": "13f4a2c34dbdeb43c0a8",
+    "url": "/static/css/main.64e46d9d.chunk.css"
+  },
+  {
+    "revision": "ea7377d035711b0a2600f34331011b74",
+    "url": "/index.html"
+  }
+];
